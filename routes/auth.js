053dced8f7f919afad098b53fa3a9838aee95d92
abrebion <abrebion@gmail.com>
2020-02-06 17:43:11 +0100

    Signin almost fixed

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const userController = require("./../controllers/user");
 const sneakerController = require("./../controllers/sneaker");
 const tagController = require("./../controllers/tag");
+const db = require("./../controllers/dbHelper");
 
 // *********************************
 // ROUTES PAS PREFIXÉES
@@ -21,14 +22,30 @@ router.get("/signin", (req, res) => {
   res.render("prod_management/signin");
 });
 
-router.post("/signin", (req, res) => {
-    res.render("prod_management/products_manage")
-})
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  console.log("----------", req.body);
+  console.log(email, password);
+
+  if (!email || !password) {
+    console.log("Missing inputs");
+    return res.redirect("/signin");
+  }
+  const user = await db.userFindByEmail(email);
+  if (!user) {
+    console.log("User does not exist");
+    return res.redirect("/signin");
+  }
+  if (bcrypt.compareSync(user.password, password)) {
+    console.log("Password does not match");
+    return res.redirect("/signin");
+  }
+  return res.render("prod_management/products_manage");
+});
 
 router.get("/products_manage", (req, res) => {
-    res.render("prod_management/products_manage")
-})
-
+  res.render("prod_management/products_manage");
+});
 
 // action : logging out
 
