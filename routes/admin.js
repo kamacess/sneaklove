3772edb0a3fd7985430665fd11751354e20282f7
Cess Flech <cecile.flechon@gmail.com>
2020-02-07 10:54:29 +0100

    route admin.js pour ajouter un produit faite

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const router = new express.Router();
-const db = require("./../controllers/dbHelper");
+const express           = require("express");
+const router            = new express.Router();
+const db                = require("./../controllers/dbHelper");
+const sneakerController = require("./../controllers/sneaker");
+const tagController     = require("./../controllers/tag")
 
 // Product Dahsboard
 router.get("/prod-manage", async (req, res) => {
@@ -12,5 +14,9 @@ router.get("/prod-manage", async (req, res) => {
 router.get("/prod-add", (req, res) => {
   res.render("prod_management/products_add");
 });
+
+router.post("/prod-add", sneakerController.create);
+
+
 
 module.exports = router;
