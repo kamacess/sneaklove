a9f2d2d582150b78c958c51813629500abcc94a0
abrebion <abrebion@gmail.com>
2020-02-07 09:20:34 +0100

    Clean some code

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("./../controllers/user");
-// const sneakerController = require('./../controllers/user')
-
 
 router.get("/", (req, res) => {
   res.render("index");
 });
-
-// router.get("/sneakers/:cat", (req, res) => {
-//   res.send("bar");
-// });
-
-// router.get("/one-product/:id", (req, res) => {
-//   res.send("baz");
-// });
-
-
-
-// router.delete('/user/:id/delete', userController.delete)
 
 module.exports = router;
